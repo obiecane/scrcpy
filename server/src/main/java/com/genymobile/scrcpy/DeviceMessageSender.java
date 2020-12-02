8e6bc9c0ce327569ef84279f2e382c60b8107826
lanjingling <lanjingl@outlook.com>
2020-12-03 01:25:18 +0800

    1

--- server/src/main/java/com/genymobile/scrcpy/DeviceMessageSender.java
+++ server/src/main/java/com/genymobile/scrcpy/DeviceMessageSender.java
@@ -2,6 +2,10 @@ package com.genymobile.scrcpy;
 
 import java.io.IOException;
 
+/**
+ * 设备消息发送器
+ * 目前只是发送粘贴板内容
+ */
 public final class DeviceMessageSender {
 
     private final DesktopConnection connection;
@@ -12,6 +16,10 @@ public final class DeviceMessageSender {
         this.connection = connection;
     }
 
+    /**
+     * 推送粘贴板中的内容到PC
+     * @param text
+     */
     public synchronized void pushClipboardText(String text) {
         clipboardText = text;
         notify();
